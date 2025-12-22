add4502e9e39a6a01486aea452f3fec7f6940b0b
yashdubey2004 <dubeyyash310@gmail.com>
2025-12-22 14:47:26 +0530

    thoda sa basic coding

---
+++ day2/Demo2/someCodes/PrintNums.java
@@ -0,0 +1,30 @@
+package day2.Demo2.someCodes;
+
+public class PrintNums {
+    public static void main(String[] args) {
+        // for(int i = 1; i <= 10; i++){
+        //     System.out.print(i + " ");
+        // // }
+        // private static int calcten(int num, int power){
+        // int result = 1;
+        // for(int i = 1; i <= power; i++){
+        // result = result * num;
+        // }
+        // return result;
+        // }   
+
+        // for(int i = 10; i > 0; i--){
+        //     int num = calcten(i, 2);
+        //     //int num = (int) Math.pow(2,i);
+        //     System.out.print(num + " ");
+        // }
+
+        int a = 10;
+        int b = 20;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("a: " + a + ", b: " + b);
+
+    }
+}
