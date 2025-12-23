7d248d865fc71a472f0fab9fa68cf9b46b636299
yashdubey2004 <dubeyyash310@gmail.com>
2025-12-23 11:07:46 +0530

    Encapsulation done

---
+++ day3/demo3/pack1/Student.java
@@ -0,0 +1,14 @@
+public class Student {
+
+    // Encapsulation and getters and setters
+    String name = "Yash";
+    private int cgpa = 11;
+
+    public int getcgpa(){
+        return cgpa;
+    }
+
+    public void setcgpa(int cgpa){
+        this.cgpa = cgpa;
+    }
+}
---
+++ day3/demo3/pack1/StudentInfo.java
@@ -0,0 +1,9 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        Student Yash = new Student();
+        System.out.println(Yash.name);
+        // Yash.setcgpa(10);
+        System.out.println(Yash.getcgpa());
+
+    }
+}
